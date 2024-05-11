c533597ddfbeae79d90cbc0a692a2655644b5784
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-11 05:33:34 +0300

    CH3

---
+++ Week1/TimeConversion/TimeConversion.c
@@ -0,0 +1,125 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+
+char* timeConversion(char* s) {
+    // Allocate memory for the result string
+    char* result = malloc(9 * sizeof(char));
+    // Store AM or PM
+    char am_pm[3];
+
+    // Parse the input string
+    int hours, minutes, seconds;
+    sscanf(s, "%d:%d:%d%s", &hours, &minutes, &seconds, am_pm);
+
+    // Convert hours to 24-hour format
+    if (strcmp(am_pm, "PM") == 0 && hours != 12) {
+        hours += 12;
+    } else if (strcmp(am_pm, "AM") == 0 && hours == 12) {
+        hours = 0;
+    }
+
+    // Format the result string
+    sprintf(result, "%02d:%02d:%02d", hours, minutes, seconds);
+
+    return result;
+}
+
+int main()
+{
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    printf("%s\n", result);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
