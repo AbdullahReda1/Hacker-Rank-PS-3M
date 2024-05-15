7fa61868aaeb9fe1c6129675e06869ba4f4cc9db
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-05-15 03:56:53 +0300

    CH5

--- Week1/CamelCase4/CamelCase4.c
+++ Week1/CamelCase4/CamelCase4.c
@@ -1,11 +1,49 @@
-//#include <stdio.h>
-//#include <string.h>
-//#include <math.h>
-//#include <stdlib.h>
-
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h> // Include ctype.h for isspace()
+#include <math.h>
+#include <stdlib.h>
+
+
+int isSpace(int c);
+int isUpperCase(char c);
+char toLowerCase(char c);
+char* ltrim(char* str);
+char* rtrim(char* str);
+void splitCamelCase(char* str, int isMethod);
+void combineCamelCase(char* str, int isClass, int variableClassNoneParentheses);
+
+
+int main() {
+    char operation, type;
+    char input[1000];
+    
+    while (scanf("%c;%c;%[^\n]%*c", &operation, &type, input) == 3) {
+        char* trimmedInput = rtrim(ltrim(input));
+        if (operation == 'S') {
+            if (type == 'C' || type == 'V') {
+                splitCamelCase(trimmedInput, 0);
+            } else if (type == 'M') {
+                splitCamelCase(trimmedInput, 1);
+            }
+            
+        } else if (operation == 'C') {
+            if (type == 'M') {
+                combineCamelCase(trimmedInput, 0, 0);
+            } else if (type == 'C') {
+                combineCamelCase(trimmedInput, 1, 1);
+            } else if (type == 'V') {
+                combineCamelCase(trimmedInput, 0, 1);
+            }
+        }
+    }
+    
+    return 0;
+}
+
+// Function to check if a character is a space
+int isSpace(int c) {
+    return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
+}
 
 // Function to check if a character is uppercase
 int isUpperCase(char c) {
@@ -15,60 +53,9 @@ int isUpperCase(char c) {
 // Function to convert a character to lowercase
 char toLowerCase(char c) {
     if (isUpperCase(c)) {
-        return c + 32; // ASCII conversion to lowercase
+        return c + 32;
     }
     return c;
-}
-
-// Function to split a camel case string into words
-void splitCamelCase(char* str) {
-    int length = strlen(str);
-    int lastCase = -1;
-    
-    for (int i = 0; i < length; i++) {
-        if (isUpperCase(str[i])) { // Using ASCII values to check for uppercase
-            if (lastCase == 0) {
-                putchar(' ');
-            }
-            putchar(toLowerCase(str[i]));
-            lastCase = 1;
-        } else {
-            putchar(str[i]);
-            lastCase = 0;
-        }
-    }
-    putchar('\n');
-}
-
-// Function to combine words into a camel case string
-void combineCamelCase(char* str, int isClass) {
-    int length = strlen(str);
-    int lastSpace = 0;
-    
-    if (isClass) { // If it's a class name, capitalize the first letter
-        putchar(str[0] >= 'a' && str[0] <= 'z' ? str[0] - 32 : str[0]); // Convert first letter to uppercase if it's lowercase
-    } else { // Otherwise, keep the first letter as it is
-        putchar(str[0]);
-    }
-    
-    for (int i = 1; i < length; i++) {
-        if (str[i] == ' ') {
-            lastSpace = 1;
-        } else {
-            if (lastSpace) { // If it's the first letter after a space, capitalize it
-                putchar(str[i] >= 'a' && str[i] <= 'z' ? str[i] - 32 : str[i]); // Convert letter to uppercase if it's lowercase
-                lastSpace = 0;
-            } else { // Otherwise, keep it lowercase
-                putchar(toLowerCase(str[i]));
-            }
-        }
-    }
-    
-    if (!isClass) { // If it's not a class name, print "()"
-        printf("()\n");
-    } else {
-        putchar('\n');
-    }
 }
 
 // Function to remove leading whitespace from a string
@@ -81,7 +68,7 @@ char* ltrim(char* str) {
         return str;
     }
 
-    while (*str != '\0' && isspace(*str)) {
+    while (*str != '\0' && isSpace(*str)) {
         str++;
     }
 
@@ -100,7 +87,7 @@ char* rtrim(char* str) {
 
     char* end = str + strlen(str) - 1;
 
-    while (end >= str && isspace(*end)) {
+    while (end >= str && isSpace(*end)) {
         end--;
     }
 
@@ -109,25 +96,64 @@ char* rtrim(char* str) {
     return str;
 }
 
-int main() {
-    char operation;
-    char type;
-    char input[1000];
+// Function to split a camel case string into words
+void splitCamelCase(char* str, int isMethod) {
+    int length = strlen(str);
+    int lastCase = -1;
     
-    while (scanf("%c;%c;%[^\n]%*c", &operation, &type, input) == 3) {
-        char* trimmed_input = rtrim(ltrim(input));
-        if (operation == 'S') {
-            if (type == 'M' || type == 'C' || type == 'V') {
-                splitCamelCase(trimmed_input);
+    for (int i = 0; i < length; i++) {
+        if (isMethod && str[i] == '(') {
+            /* Skip parentheses if it's a method name */
+            break;
+        } else if (isUpperCase(str[i])) {
+            /* Using ASCII values to check for uppercase */
+            if (lastCase == 0) {
+                putchar(' ');
             }
-        } else if (operation == 'C') {
-            if (type == 'M') {
-                combineCamelCase(trimmed_input, 0);
-            } else if (type == 'C' || type == 'V') {
-                combineCamelCase(trimmed_input, type == 'C' ? 1 : 0);
+            putchar(toLowerCase(str[i]));
+            lastCase = 1;
+        } else {
+            putchar(str[i]);
+            lastCase = 0;
+        }
+    }
+    putchar('\n');
+}
+
+// Function to combine words into a camel case string
+void combineCamelCase(char* str, int isClass, int variableClassNoneParentheses) {
+    int length = strlen(str);
+    int lastSpace = 0;
+    
+    /* If it's a class name, capitalize the first letter */
+    if (isClass) {
+        /* Convert first letter to uppercase if it's lowercase */
+        putchar(str[0] >= 'a' && str[0] <= 'z' ? str[0] - 32 : str[0]);
+    } else if (!isClass || variableClassNoneParentheses){
+        /* Otherwise, keep the first letter as it is */
+        putchar(str[0]);
+    }
+    
+    for (int i = 1; i < length; i++) {
+        if (str[i] == ' ') {
+            lastSpace = 1;
+        } else {
+            /* If it's the first letter after a space, capitalize it */
+            if (lastSpace) {
+                /* Convert letter to uppercase if it's lowercase */
+                putchar(str[i] >= 'a' && str[i] <= 'z' ? str[i] - 32 : str[i]);
+                lastSpace = 0;
+            } else {
+                /* Otherwise, keep it lowercase */
+                putchar(toLowerCase(str[i]));
             }
         }
     }
     
-    return 0;
+    /* Not need Parentheses */
+    if (!variableClassNoneParentheses) {
+        printf("()\n");
+    } else {
+        putchar('\n');
+    }
 }
